3709fee9218c91e3ebb08a895f94dab324aab536
Diorgenes Morais <10004801@edp.com.br>
2024-03-14 10:32:57 -0300

    first commit

---
+++ src/test/java/com/dms/sale/SaleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dms.sale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
